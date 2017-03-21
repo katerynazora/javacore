d1d96e98fe6792a36e921ccde1ce4af614ce6fc0
katerynazora <kateryna.zora@gmail.com>
2017-03-21 19:07:21 +0200

    add unit tests Car.java, CarRunner.java, CircleArea.java, CircleAreaRunner.java, readability of pom.xml

--- src/main/java/com/zora/app/classwork/lesson4/CircleArea.java
+++ src/main/java/com/zora/app/classwork/lesson4/CircleArea.java
@@ -6,11 +6,12 @@ package com.zora.app.classwork.lesson4;
 public class CircleArea {
     static int more = 12;
     static int less = 15;
-    public static void calculateBigger(){
+    public static void calculateBigger() {
         if (more > less) {
             System.out.println("More is bigger");
         } else {
             System.out.println("it's can be possible");
         }
     }
+
 }
